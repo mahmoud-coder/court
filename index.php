af030618a6a3b4c40cf1f44117ed1d3e2fac249c
Mahmoud Sami <mahmoudsamyessawy@gmail.com>
2019-01-20 20:41:20 +0200

    adding the HTML for document type

--- index.php
+++ index.php
@@ -31,12 +31,30 @@
                 </div>
                 <!-- the row of radio bottons -->
                 <div class="form-row">
+                    <!-- the kind of document -->
+                    <div class="col mb-3">
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="document1" name="document" value="absent" class="custom-control-input">
+                            <label class="custom-control-label" for="document1">غيابي</label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="document2" name="document" value="slef" class="custom-control-input">
+                            <label class="custom-control-label" for="document2">بشخصه</label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="document3" name="document" value="agent" class="custom-control-input">
+                            <label class="custom-control-label" for="document3">بوكيل</label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="document4" name="document" value="attend_absent" class="custom-control-input">
+                            <label class="custom-control-label" for="document4">غيابي + حضور</label>
+                        </div>
+                    </div>
                     <!-- the kind of plaintiff -->
                     <div class="col mb-3">
                         <div class="custom-control custom-radio">
                             <input type="radio" id="customRadio1" name="plaintiff" value="male" class="custom-control-input">
                             <label class="custom-control-label" for="customRadio1">مدعي</label>
-
                         </div>
                         <div class="custom-control custom-radio">
                             <input type="radio" id="customRadio2" name="plaintiff" value="female" class="custom-control-input">
@@ -61,31 +79,31 @@
                     </div>
                     <!-- the kind of defendant -->
                     <div class="col mb-3">
-                            <div class="custom-control custom-radio">
-                                    <input type="radio" id="customRadio7" name="defendant" value="male" class="custom-control-input">
-                                    <label class="custom-control-label" for="customRadio7">مدعي عليه</label>
-        
-                                </div>
-                                <div class="custom-control custom-radio">
-                                    <input type="radio" id="customRadio8" name="defendant" value="female" class="custom-control-input">
-                                    <label class="custom-control-label" for="customRadio8">مدعي عليها</label>
-                                </div>
-                                <div class="custom-control custom-radio">
-                                    <input type="radio" id="customRadio9" name="defendant" value="two_male" class="custom-control-input">
-                                    <label class="custom-control-label" for="customRadio9">مدعي عليهما</label>
-                                </div>
-                                <div class="custom-control custom-radio">
-                                    <input type="radio" id="customRadio10" name="defendant" value="two_female" class="custom-control-input">
-                                    <label class="custom-control-label" for="customRadio10">مدعي عليهنا</label>
-                                </div>
-                                <div class="custom-control custom-radio">
-                                    <input type="radio" id="customRadio11" name="defendant" value="many_male" class="custom-control-input">
-                                    <label class="custom-control-label" for="customRadio11">مدعي عليهم</label>
-                                </div>
-                                <div class="custom-control custom-radio">
-                                    <input type="radio" id="customRadio12" name="defendant" value="many_female" class="custom-control-input">
-                                    <label class="custom-control-label" for="customRadio12">مدعي عليهن</label>
-                                </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio7" name="defendant" value="male" class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio7">مدعي عليه</label>
+
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio8" name="defendant" value="female" class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio8">مدعي عليها</label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio9" name="defendant" value="two_male" class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio9">مدعي عليهما</label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio10" name="defendant" value="two_female" class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio10">مدعي عليهنا</label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio11" name="defendant" value="many_male" class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio11">مدعي عليهم</label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio12" name="defendant" value="many_female" class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio12">مدعي عليهن</label>
+                        </div>
                     </div>
                 </div>
                 <div class="form-group">
@@ -104,7 +122,7 @@
         $(function () {
             $("#main_form").submit(function (e) {
                 e.preventDefault();
-                $.post("./app.php", $("#main_form").serialize() );
+                $.post("./app.php", $("#main_form").serialize());
                 $("[name='index']").val(function (i, oldValue) { return parseInt(oldValue) + 1; });
                 $("[name='case_number']").val('');
                 $("[name='document_date']").val('');
